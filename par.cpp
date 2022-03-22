bf3ef3a2e67ad307da723d865c2f7c79a1792f7b
PeanutSouth <101263125+PeanutSouth@users.noreply.github.com>
2022-03-22 09:37:18 -0300

    Update par.cpp

--- par.cpp
+++ par.cpp
@@ -7,21 +7,21 @@ using namespace std;
 int main(){
     
 int num;
-
 cout << "ingresar numero:"<<endl;
 cin>>num;
-
-
+if (num<-1){
+    while (num<-1){
+        num=num+2;
+    }
+}    
 while (num>=1){
-
-num=num-2;
-
+    num=num-2;
 }
-
 if (num==0){
     cout<<"el numero es par "<<endl;
 }
 else{
     cout<<"el numero es impar "<<endl;
 }
+    
 }
