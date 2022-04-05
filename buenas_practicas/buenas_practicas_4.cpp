3fc24c2f99a740ea0a59a4077d984756b87240fa
PeanutSouth <101263125+PeanutSouth@users.noreply.github.com>
2022-04-05 12:36:50 -0300

    Add files via upload
    5/4

--- buenas_practicas/buenas_practicas_4.cpp
+++ buenas_practicas/buenas_practicas_4.cpp
@@ -4,11 +4,18 @@ using namespace std;
 
 int main(){
 int num_1;
-int resto_1;
-int figo;
+int figo1=0;
+int figo2=1;
+int figo3;
+int l=99;
 cout<<"ingresar_numero\n";
 cin>>num_1;
-  while (num_1==figo)
-
-
+while (num_1!=figo3){
+    num_1++;
+    figo3=figo2+figo1;
+    figo1=figo2;
+    figo2=figo3;
+}
+cout<<num_1
+}
 
